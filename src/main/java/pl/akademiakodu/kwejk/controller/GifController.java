859adc1195512d99df2fa9c622bbaaeaf794b29f
Jacek Raczynski <jacquest@tlen.pl>
2017-09-06 10:15:56 +0200

    remove unused imports GifCOntroller

--- src/main/java/pl/akademiakodu/kwejk/controller/GifController.java
+++ src/main/java/pl/akademiakodu/kwejk/controller/GifController.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-import pl.akademiakodu.kwejk.ErrorMessage;
 import pl.akademiakodu.kwejk.dao.GifDao;
 
 
