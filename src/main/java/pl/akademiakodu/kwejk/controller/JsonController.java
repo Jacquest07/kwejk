a029cd9e2c49caf074cc3ecf0d818a9102a9af8e
Jacek Raczynski <jacquest@tlen.pl>
2017-09-06 12:48:44 +0200

    Created Json for any gif

--- src/main/java/pl/akademiakodu/kwejk/controller/JsonController.java
+++ src/main/java/pl/akademiakodu/kwejk/controller/JsonController.java
@@ -1,10 +1,14 @@
 package pl.akademiakodu.kwejk.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import pl.akademiakodu.kwejk.Category;
+import pl.akademiakodu.kwejk.Gif;
 import pl.akademiakodu.kwejk.dao.CategoryDao;
+import pl.akademiakodu.kwejk.dao.GifDao;
 
 import java.util.List;
 
@@ -13,15 +17,25 @@ import java.util.List;
  */
 
 @RestController
+@RequestMapping("/api")
 public class JsonController
 {
 
     @Autowired
     private CategoryDao categoryDao;
 
-    @RequestMapping("/api/categories")
+    @Autowired
+    private GifDao gifDao;
+
+    @RequestMapping("/categories")
     public List<Category> categoriesJson()
     {
         return categoryDao.findAll();
+    }
+
+    @RequestMapping("/gifs{name}")
+    public Gif gifJson(@RequestParam String q)
+    {
+        return gifDao.findOne(q);
     }
 }
